846bd184d9b03b2f3c15954d63fce7e744f6813b
Nyoko <Nyoko@DESKTOP-3UJV09O>
2025-06-11 11:36:16 -0600

    v2.2R1 Quick fix for time of day slider.

--- XML/GlobalPaths.cs
+++ XML/GlobalPaths.cs
@@ -67,7 +67,7 @@ namespace Lumina.XML
         /// <summary>
         /// Returns the version.
         /// </summary>
-        public static string Version = "v2.2";
+        public static string Version = "v2.2R1";
 
         /// <summary>
         /// SupportedGameVersion, returns the supported game version.
