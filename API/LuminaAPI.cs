307e9d53c2bce51e2e62de9af25a95072466c7c5
Nyoko <Nyoko@DESKTOP-3UJV09O>
2025-10-26 11:28:59 -0600

    Provide other users with the GlobalVariables reference.

---
+++ API/LuminaAPI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace LuminaMod.API
+{
+    /// <summary>
+    /// Exposes Lumina GlobalVariables to other mods via a discoverable GameObject.
+    /// </summary>
+    public class LuminaAPI : MonoBehaviour
+    {
+        public const string API_OBJECT_NAME = "LuminaAPI";
+
+        /// <summary>
+        /// Runtime GlobalVariables reference.
+        /// </summary>
+        public LuminaMod.XML.GlobalVariables Data
+            => LuminaMod.XML.GlobalVariables.Instance;
+
+        private void Awake()
+        {
+            // Prevent duplicates
+            if (GameObject.Find(API_OBJECT_NAME) != null && gameObject.name != API_OBJECT_NAME)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            gameObject.name = API_OBJECT_NAME;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
